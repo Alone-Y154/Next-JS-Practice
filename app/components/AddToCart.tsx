880d24a239e14355ae0017801aea4d5af9077643
Yashwanth <Yashwanth@tetraflow.tech>
2024-07-01 03:34:45 +0530

    Tailwind css

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const AddToCart = () => {
   return (
     <div>
-      <button onClick={()=> console.log('clicked')}>Add to Cart</button>
+      <button className='bg-sky-500 p-2 rounded text-white border cursor-pointer' onClick={()=> console.log('clicked')}>Add to Cart</button>
     </div>
   )
 }
